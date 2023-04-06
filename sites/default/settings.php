35dbb467f306bb689f83aedcb080721170f5d911
adriancpp <adrian.cpp@wp.pl>
2023-04-06 22:20:19 +0200

    finished installation

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'drupal7test',
+      'username' => 'root',
+      'password' => 'root',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Quoting of identifiers in MySQL.
@@ -294,7 +308,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'q8dGbSUvCpl9nVAhuKEiT_RHEr0y0eO9pHZOJ53zjj8';
 
 /**
  * Base URL (optional).
